5d26d6e16b1b4882421f96bc4e5af50da20141df
Herri Purba <herripurba@gmail.com>
2020-03-24 17:38:17 +0700

    create table move Transition and move Status

---
+++ src/chaturanga/player/MoveStatus.java
@@ -0,0 +1,38 @@
+package chaturanga.player;
+
+public enum MoveStatus {
+    DONE {
+        @Override
+        public boolean isDone(){
+            return  true;
+        }
+        @Override
+        public boolean isInCheckMate(){
+            return  false;
+        }
+    },
+    IlLEGAL_MOVE {
+        @Override
+        public boolean isDone(){
+            return  false;
+        }
+        @Override
+        public boolean isInCheckMate(){
+            return  false;
+        }
+    },
+    LEAVES_PLAYER_IN_CHECK {
+        @Override
+        public boolean isDone(){
+            return  false;
+        }
+        @Override
+        public boolean isInCheckMate(){
+            return  true;
+        }
+    };
+
+    public abstract boolean isDone();
+
+    public abstract boolean isInCheckMate();
+}
